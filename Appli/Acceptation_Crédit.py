0e7f408f0aa6ac05aa9bc867c8c003a7e8f64594
m322345 <cedric@ewd.fr>
2025-01-25 22:14:39 +0100

    upd

--- Appli/Acceptation_Crédit.py
+++ Appli/Acceptation_Crédit.py
@@ -239,10 +239,9 @@ def main():
                         marker_size = marker_size / 2
                         fig.add_scatter(x=dfGraph.index,
                                         y=dfGraph['client_prc'], mode="markers",
-                                        text=id_client,
                                         marker=dict(size=marker_size, color="#464646"),
-                                        hovertemplate="Client %{text}<br>Valeur: %{y:.3f}<extra></extra>",
-                                        name="Client %{text}")
+                                        hovertemplate="Client "+id_client+"<br>Valeur: %{y:.3f}<extra></extra>",
+                                        name="Client "+id_client)
                         #fig.update_yaxes(showspikes=True, spikecolor=CouleurAccord, spikesnap="cursor", spikemode="across")
                         st.plotly_chart(fig, use_container_width=False, theme="streamlit", on_select="ignore")
                         #fig = go.Figure()
