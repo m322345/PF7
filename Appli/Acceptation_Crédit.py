b1f48eab499fafb848fd00e190066c426eacf9f1
m322345 <cedric@ewd.fr>
2025-01-25 20:24:55 +0100

    test

--- Appli/Acceptation_Crédit.py
+++ Appli/Acceptation_Crédit.py
@@ -240,8 +240,8 @@ def main():
                         fig.add_scatter(x=dfGraph.index,
                                         y=dfGraph['client_prc'], mode="markers",
                                         marker=dict(size=marker_size, color="#464646"),
-                                        hovertemplate="Client "+id_client+"<br>Valeur: %{y:.3f}<extra></extra>",
-                                        name="Client "+id_client)
+                                        hovertemplate=f"Client {id_client}<br>Valeur: {y:.3f}<extra></extra>",
+                                        name=f"Client {id_client}")
                         st.plotly_chart(fig, use_container_width=False, theme="streamlit", on_select="ignore")
                         #fig = go.Figure()
                         #fig.add_trace(data)
